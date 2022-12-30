d046fea9595c3588c47d517d393c9587e374f0f7
Juche <azhucheng1@qq.com>
2022-12-30 12:37:27 +0800

    66 => BigInt 加法暴力计算

---
+++ 66.加一.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=66 lang=javascript
+ *
+ * [66] 加一
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function (digits) {
+  return String.prototype.split.call(BigInt(digits.join('')) + BigInt(1), '');
+};
+
+// [6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3];
+// @lc code=end
